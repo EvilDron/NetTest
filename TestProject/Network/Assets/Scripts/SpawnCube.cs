cbe3612ddc4cdede4cc8ae891b9d6213a01d7d2c
Lol <Loler@gmail.com>
2018-05-14 17:39:24 +0300

    Test

---
+++ TestProject/Network/Assets/Scripts/SpawnCube.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class SpawnCube : NetworkBehaviour {
+
+    [SerializeField]
+    List<GameObject> objectsToSpawn;
+
+    [SerializeField]
+    Transform spawnPoint;
+	
+	void Start () {
+        spawnPoint = GameObject.Find("Spawnpoint").transform;
+
+    }
+    private void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CmdSpawnObj(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            CmdSpawnObj(1);
+        }
+    }
+
+    [Command]
+    public void CmdSpawnObj(int id)
+    {
+        RpcSpawmObj(id);
+    }
+
+    [ClientRpc]
+    void RpcSpawmObj(int id)
+    {
+        Instantiate(objectsToSpawn[id], spawnPoint.position, spawnPoint.rotation);
+    }
+}
---
+++ TestProject/Test/Assets/Push.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Push : MonoBehaviour {
+    public float force = 10f;
+    Rigidbody rg;
+	void Start () {
+        rg = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rg.AddForce(new Vector3(force, 0, 0));
+        }
+	}
+}
